97f02eb6cab493d80d98c5081827016ddfccd144
unknown <dipanshu765@gmail.com>
2022-07-05 23:40:07 +0530

    Finished Login and Register Pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { login, reset } from '../features/auth/authSlice'
 import Spinner from '../components/Spinner'
+
+import './Login.css'
 
 const Login = () => {
   const [ formData, setFormData ] = useState({
@@ -54,46 +56,52 @@ const Login = () => {
   }
 
   return (
-    <React.Fragment>
-        <section className='heading'>
-            <h1>
-                <FaSignInAlt /> Login
-            </h1>
-            <p>LOGIN AND START PLAYING YOUR FAVOURITE MUSIC</p>
-        </section>
+    <div className='login-form' style={{'width': '100vw', 'height': '100vh'}}>
+      <div className='login-form-container'>
+        <h1>
+          <span className='icon'><FaSignInAlt /></span><span className='text'>Login</span>
+        </h1>
+        <p>LOGIN AND START PLAYING YOUR FAVOURITE MUSIC</p>
 
-        <section className='form'>
-            <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                    <input
-                    type="email"
-                    className='form-control' 
-                    id='email' 
-                    name='email' 
-                    value={email} 
-                    placeholder='Enter your Email'
-                    onChange={onChange} 
-                    />
-                </div>
-                <div className='form-group'>
-                    <input
-                    type="password"
-                    className='form-control' 
-                    id='password' 
-                    name='password' 
-                    value={password} 
-                    placeholder='Enter your Password'
-                    onChange={onChange} 
-                    />
-                </div>
-                <div className='form-group'>
-                    <button type='submit' className='btn btn-block'>
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </section>
-    </React.Fragment>
+        <form className='form' onSubmit={onSubmit}>
+          <div className='form-group'>
+              <input
+              type="email"
+              className='form-control' 
+              id='email' 
+              name='email' 
+              value={email} 
+              placeholder='Enter your Email'
+              onChange={onChange}
+              required 
+              />
+          </div>
+          <div className='form-group'>
+              <input
+              type="password"
+              className='form-control' 
+              id='password' 
+              name='password' 
+              value={password} 
+              placeholder='Enter your Password'
+              onChange={onChange} 
+              required
+              />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+                SUBMIT
+            </button>
+            <p>DON'T HAVE AN ACCOUNT?</p>
+            <button className='btn btn-block'>
+              <Link to='../register'>
+                SIGN UP
+              </Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,112 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate, Link } from 'react-router-dom'
+import { FaSignInAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+
+import { reset, register } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+import './Register.css'
 
 const Register = () => {
+  const [ formData, setFormData ] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  
+  const { name, email, password } = formData
+  const { user, isLoading, isError, isSuccess, message } = useSelector( state => state.auth )
+
+  useEffect(()=>{
+    if(isError) {
+      toast.error('message')
+    }
+
+    if(isSuccess || user) 
+      navigate('/')
+
+    dispatch(reset())
+  }, [user, isLoading, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = e => {
+    setFormData(prevState => ({
+        ...prevState,
+        [e.target.name]: e.target.value
+      })
+    )
+  }
+
+  const onSubmit = e => {
+    dispatch(register(formData))
+    e.preventDefault()
+  }
+
+  if(isLoading) {
+    return <Spinner />
+  }
+  
   return (
-    <div>Register</div>
+    <div className='register-form' style={{'width': '100vw', 'height': '100vh'}}>
+      <div className='register-form-container'>
+        <h1>
+          <span className='icon'><FaSignInAlt /></span><span className='text'>SIGN UP</span>
+        </h1>
+        <p>SIGN UP AND START PLAYING YOUR FAVOURITE MUSIC</p>
+
+        <form className='form' onSubmit={onSubmit}>
+          <div className='form-group'>
+              <input
+              className='form-control' 
+              id='name' 
+              name='name' 
+              value={name} 
+              placeholder='Enter your Name'
+              onChange={onChange} 
+              required
+              />
+          </div>
+          <div className='form-group'>
+              <input
+              type="email"
+              className='form-control' 
+              id='email' 
+              name='email' 
+              value={email} 
+              placeholder='Enter your Email'
+              onChange={onChange} 
+              required
+              />
+          </div>
+          <div className='form-group'>
+              <input
+              type="password"
+              className='form-control' 
+              id='password' 
+              name='password' 
+              value={password} 
+              placeholder='Enter your Password'
+              onChange={onChange} 
+              required
+              />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+                SUBMIT
+            </button>
+            <p>ALREADY HAVE AN ACCOUNT?</p>
+            <button className='btn btn-block'>
+              <Link to='../login'>
+                LOGIN
+              </Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
